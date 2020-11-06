c5bf3d4a21e696563e322f9af4b57bdd10bca60b
lish <lishanghan@126.com>
2020-11-06 09:25:47 +0800

    接入钉钉发消息

---
+++ src/main/java/com/hry/config/DingProperty.java
@@ -0,0 +1,10 @@
+package com.hry.config;
+
+/**
+ * @author lish
+ * @date 2020/11/5 9:22
+ * @description:TODO
+ * @since 1.8
+ */
+public class DingProperty {
+}
---
+++ src/main/java/com/hry/util/DingUtil.java
@@ -0,0 +1,10 @@
+package com.hry.util;
+
+/**
+ * @author lish
+ * @date 2020/11/5 15:50
+ * @description:TODO
+ * @since 1.8
+ */
+public class DingUtil {
+}
